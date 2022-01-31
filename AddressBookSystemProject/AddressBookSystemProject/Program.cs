16ac968337126935641b7a3c8bcdefc380f8a126
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-31 20:06:47 +0530

    welcometoaddressbooksystem

---
+++ AddressBookSystemProject/AddressBookSystemProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome AddressBookSystem");
+            Console.ReadLine();
+        }
+    }
+}
