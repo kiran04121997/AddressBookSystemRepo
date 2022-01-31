8168c8e84ab1c7a9c437140bb143313fc93f0692
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-31 20:22:08 +0530

    add the data

--- AddressBookSystemProject/AddressBookSystemProject/Program.cs
+++ AddressBookSystemProject/AddressBookSystemProject/Program.cs
@@ -10,7 +10,25 @@ namespace AddressBookSystemProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome AddressBookSystem");
+            //Main  Method / program Entry Point
+                //Showing Welcome Message On console.
+                Console.WriteLine("Welcome to Address Book Progam \n");
+
+                Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+                CreateAddressBookSystem addressBook = new CreateAddressBookSystem()  //Initializing elements using collection-initializer syntax
+                {
+                    FirstName = Console.ReadLine(),
+                    LastName = Console.ReadLine(),
+                    Address = Console.ReadLine(),
+                    City = Console.ReadLine(),
+                    State = Console.ReadLine(),
+                    Zip = Convert.ToDouble(Console.ReadLine()),
+                    PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                    Email = Console.ReadLine(),
+                };
+                addressBook.displayContact();
+                Console.ReadLine();
+                Console.WriteLine("Welcome AddressBookSystem");
             Console.ReadLine();
         }
     }
