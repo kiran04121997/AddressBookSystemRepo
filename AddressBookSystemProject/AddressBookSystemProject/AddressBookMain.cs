14e318008d1e5c2cb3e19dcad7da19fc6f5d1a72
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-02-21 22:43:30 +0530

    update

--- AddressBookSystemProject/AddressBookSystemProject/AddressBookMain.cs
+++ AddressBookSystemProject/AddressBookSystemProject/AddressBookMain.cs
@@ -10,10 +10,14 @@ namespace AddressBookSystemProject
     {
         List<Contact> contactList;
         Dictionary<string, AddressBook> addressBookDict;
+        Dictionary<string, List<Contact>> city_Person;
+        Dictionary<string, List<Contact>> state_Person;
         public AddressBook()
         {
             contactList = new List<Contact>();
             addressBookDict = new Dictionary<string, AddressBook>();
+            city_Person = new Dictionary<string, List<Contact>>();
+            state_Person = new Dictionary<string, List<Contact>>();
         }
 
         // UC1 - Create Contacts in address book
@@ -23,39 +27,43 @@ namespace AddressBookSystemProject
             addressBookDict[bookName].contactList.Add(personDetails);
         }
 
-        //UC2 - Add New Contact Details
+        //UC2 - Add New Contact Details, UV7 - Avoid duplicate entry by firstNmae
         public void AddNewContact(string bookName)
         {
-            Console.WriteLine("Enter your First Name: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter your Last Name: ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter your Address: ");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter your City: ");
-            string city = Console.ReadLine();
-            Console.WriteLine("Enter your State: ");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter your Zipcode: ");
-            int zipcode = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your Phone Number: ");
-            long phoneNumber = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("Enter your EmailID: ");
-            string email = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Enter your First Name: ");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter your Last Name: ");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter your Address: ");
+                string address = Console.ReadLine();
+                Console.WriteLine("Enter your City: ");
+                string city = Console.ReadLine();
+                Console.WriteLine("Enter your State: ");
+                string state = Console.ReadLine();
+                Console.WriteLine("Enter your Zipcode: ");
+                int zipcode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter your Phone Number: ");
+                long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter your EmailID: ");
+                string email = Console.ReadLine();
 
-            int x = 0;
-            for (int i = 0; i < addressBookDict[bookName].contactList.Count; i++)
-            {
-                if (addressBookDict[bookName].contactList[i].firstName.Equals(firstName))
-                    x++;
+                var res = addressBookDict[bookName].contactList.Find(p => p.firstName.Equals(firstName));
+                if (res != null)
+                {
+                    Console.WriteLine("Duplicate contacts not allowed");
+                }
+                else
+                {
+                    AddContactDetails(firstName, lastName, address, city, state, zipcode, phoneNumber, email, bookName);
+                    ViewContacts(bookName);
+                }
             }
-            if (x == 0)
+            catch (Exception ex)
             {
-                AddContactDetails(firstName, lastName, address, city, state, zipcode, phoneNumber, email, bookName);
-                ViewContacts(bookName);
+                Console.WriteLine(ex.Message);
             }
-            else
-                Console.WriteLine("Duplicate contacts not allowed");
         }
 
         //View contacts of a address book
@@ -64,7 +72,7 @@ namespace AddressBookSystemProject
             Console.WriteLine($"Current AddressBook: {bookName}");
             for (int i = 0; i < addressBookDict[bookName].contactList.Count; i++)
             {
-                Console.WriteLine(i + 1 + ": " + addressBookDict[bookName].contactList[i].firstName + " " + addressBookDict[bookName].contactList[i].lastName);
+                Console.WriteLine(i+1 + ": " + addressBookDict[bookName].contactList[i].firstName + " " + addressBookDict[bookName].contactList[i].lastName);
             }
         }
 
@@ -76,7 +84,7 @@ namespace AddressBookSystemProject
             {
                 if (addressBookDict[bookName].contactList[i].firstName == f_Name)
                 {
-                    Console.WriteLine("contact No. {0}: ", i + 1);
+                    Console.WriteLine("contact No. {0}: ", i+1);
                     Console.WriteLine("First Name: " + addressBookDict[bookName].contactList[i].firstName);
                     Console.WriteLine("Last Name: " + addressBookDict[bookName].contactList[i].lastName);
                     Console.WriteLine("Address: " + addressBookDict[bookName].contactList[i].address);
@@ -170,7 +178,7 @@ namespace AddressBookSystemProject
             {
                 AddressBook addressBook = new AddressBook();
                 addressBookDict.Add(newAddressBook, addressBook);
-                Console.WriteLine("AddressBook {0} Created Successfully.", newAddressBook);
+                Console.WriteLine("AddressBook {0} Created Successfully.",newAddressBook);
             }
         }
 
@@ -178,9 +186,9 @@ namespace AddressBookSystemProject
         {
             foreach (var book in addressBookDict)
             {
-                Console.WriteLine(book.Key);
+                Console.WriteLine(book.Key); 
             }
-
+            
         }
 
         public string CheckAddressBook(string adBookName)
@@ -198,21 +206,99 @@ namespace AddressBookSystemProject
         //UC8 - Search Person By City Or State
         public void SearchPersonByCityOrState(string userData)
         {
-            int x = 0;
             foreach (var book in addressBookDict)
             {
-                for (int i = 0; i < book.Value.contactList.Count; i++)
+                var searchResult = book.Value.contactList.FindAll(x => x.city == userData || x.state == userData);
+                if(searchResult.Count != 0)
                 {
-                    if (book.Value.contactList[i].city.Equals(userData) || book.Value.contactList[i].state.Equals(userData))
+                    foreach (var item in searchResult)
                     {
-                        Console.WriteLine(book.Value.contactList[i].ToString());
-                        x++;
+                        Console.WriteLine(item.ToString());
                     }
                 }
+                else
+                    Console.WriteLine("No person found for this city or state");
             }
-            Console.WriteLine($"\nNumber of Persons for {userData} are : {x}");
-            if (x == 0)
-                Console.WriteLine("No person found for this city or state");
+        }
+
+        //UC9 View Person By City Or State 
+        public void ViewPersonByCityOrState()
+        {
+            Console.WriteLine("Choose an option \n1. View Person by city \n2. View Person by state");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the city");
+                    string city = Console.ReadLine();
+                    foreach (var book in addressBookDict)
+                    {
+                        var cityResult = book.Value.contactList.FindAll(x => x.city == city);
+                        if (cityResult.Count != 0)
+                        {
+                            city_Person.Add(city, cityResult);
+                            foreach (var item in city_Person[city])
+                            {
+                                Console.WriteLine(item.ToString());
+                            }
+                        }
+                        else
+                            Console.WriteLine("No person found for this city");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the state");
+                    string state = Console.ReadLine();
+                    foreach (var book in addressBookDict)
+                    {
+                        var stateResult = book.Value.contactList.FindAll(x => x.state == state);
+                        if (stateResult.Count != 0)
+                        {
+                            state_Person.Add(state, stateResult);
+                            foreach (var item in state_Person[state])
+                            {
+                                Console.WriteLine(item.ToString());
+                            }
+                        }
+                        else
+                            Console.WriteLine("No person found for this state");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Choose correct option");
+                    break;
+            }
+        }
+
+        //UC10 Number of Persons count By City Or State 
+        public void CountPersonByCityOrState()
+        {
+            Console.WriteLine("Choose an option \n1. Person count by city \n2. Person count by state");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the city");
+                    string city = Console.ReadLine();
+                    foreach (var book in addressBookDict)
+                    {
+                        var cityResult = book.Value.contactList.FindAll(x => x.city == city);
+                        Console.WriteLine($"Person count by city- {city}: "+cityResult.Count);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the state");
+                    string state = Console.ReadLine();
+                    foreach (var book in addressBookDict)
+                    {
+                        var stateResult = book.Value.contactList.FindAll(x => x.state == state);
+                        Console.WriteLine($"Person count by state- {state}: "+stateResult.Count);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Choose correct option");
+                    break;
+            }
         }
     }
 }
